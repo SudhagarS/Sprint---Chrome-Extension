79d2843c46a707cacde5053c1fc93721b3602666
SudhagarS <sudhagar@isudhagar.in>
2014-09-01 03:41:28 +0530

    background.js moved to js/

---
+++ js/background.js
@@ -0,0 +1,35 @@
+var callback = function (details) {
+    console.log("Intercepted: " + details.url);
+    return {cancel: true};
+}
+
+var opt_extraInfoSpec = ["blocking"];
+
+function initSprint(blockedUrls) {
+    console.log('Sprint icon clicked');
+    console.log(blockedUrls);
+    chrome.browserAction.setIcon({
+        path: "images/icon2.png"
+    });
+    chrome.webRequest.onBeforeRequest.addListener(callback, {urls: processUrls(blockedUrls)}, opt_extraInfoSpec);
+    chrome.alarms.create("SprintAlarm", {when: Date.now() + 10000});
+    
+    chrome.alarms.onAlarm.addListener(function (alarm) {
+        console.log('alarm called');
+        chrome.webRequest.onBeforeRequest.removeListener(callback);
+        chrome.browserAction.setIcon({
+            path: "images/icon.png"
+        });
+    });
+    console.log('alarm created');
+}
+
+
+
+function processUrls(urls){
+    var urlsProcessed = urls.slice()
+    for(var i=0; i<urlsProcessed.length; i++){
+        urlsProcessed[i] = "*://" + urlsProcessed[i] + "/*";
+    }
+    return urlsProcessed;
+}
